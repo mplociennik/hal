7284183240bac4f196abb8258acfb14f3734020a
Cieniu <marcin@laret.pl>
2017-06-19 22:09:34 +0200

    debug

--- websocket_server.py
+++ websocket_server.py
@@ -2,7 +2,7 @@
 import sys
 import json
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
-from pymove import PyMove
+from PyMove import PyMove
 
 class HalEcho(WebSocket):
 
@@ -11,16 +11,21 @@ class HalEcho(WebSocket):
         state = 'Direction: {0}, State {1}'.format(direction, state)
         print state
         if "up" in direction:
-            # Pymove().run_up_start()
-            state = 'Move UP state: {0}'.format(state)        
+            try:
+                PyMove().run_up_start() if state else PyMove().run_up_stop()
+                state = 'Move UP state: {0}'.format(state)     
+            except:
+                print "Unexpected error:", sys.exc_info()[0]
+                raise
+
         if 'down' in direction:
-            # Pymove().run_down_start() if state else Pymove().run_down_stop()
+            PyMove().run_down_start() if state else PyMove().run_down_stop()
             state = 'Move DOWN state: {0}'.format(state)
         if 'left' in direction:
-            # Pymove().run_left_start() if state else Pymove().run_left_stop()
+            PyMove().run_left_start() if state else PyMove().run_left_stop()
             state = 'Move LEFT state: {0}'.format(state)        
         if 'right' in direction:
-            # Pymove().run_right_start() if state else move.run_right_stop()
+            PyMove().run_right_start() if state else move.run_right_stop()
             state = 'Move RIGHT state: {0}'.format(state)
         return state
 

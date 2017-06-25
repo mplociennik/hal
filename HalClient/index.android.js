ffc252c6ba58b5aef289083d8de63b4e4bcd75fc
Cieniu <marcin@laret.pl>
2017-06-25 21:24:54 +0200

    fix client and update

--- HalClient/index.android.js
+++ HalClient/index.android.js
@@ -1,60 +1,60 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
- import React, { Component } from 'react';
- import {
+import React, { Component } from 'react';
+import {
   AppRegistry,
   StyleSheet,
   Text,
   View,
   TouchableWithoutFeedback,
-  Button
+  Button,
+  Switch
 } from 'react-native';
 
 export default class HalClient extends Component {
 
   constructor(props){
     super(props);
-    this.state = {socketStream: null, moveDirection: null, moveState: null, socketResponse: null, protectHomeState: false};
+    this.state = {moveDirection: null, moveState: null, socketResponse: null, protectHomeState: false};
+    this.socketStream = null;
   }
 
   componentDidMount(){
-    var self = this;
-    this.state.socketStream.onmessage = (request)=>{
-      console.log(request.data);
-      self.setState({socketResponse: request.data)});
-    };    
-
-    this.state.socketStream.onclose = (e)=>{
-      console.log('Socket connection closed.');
-      console.log(e.code, e.reason);
-      self.setState({socketResponse: 'Socket connection closed.'});
-    };
+    this._connectSocket();
   }
 
   _connectSocket(){
-    const socketStream = new WebSocket("ws://127.0.0.1:8083");
-    this.setState({socketStream: socketStream});
+    console.log('Starting connection...');
+    var self = this;
+    this.socketStream = new WebSocket("ws://192.168.1.151:8083");
+    console.log(this.socketStream);
+
+    this.socketStream.onmessage = (request)=>{
+      console.log(request.data);
+      self.setState({socketResponse: request.data});
+    };    
+
+    this.socketStream.onclose = (e)=>{
+      console.log('Socket connection closed.');
+      console.log(e.code, e.reason);
+      self.setState({socketResponse: 'Socket connection closed.'});
+    
+    };
   }
 
   _disconnectSocket(){
-    this.state.socketStream.close();
+    this.socketStream.close();
   }
 
   _move(direction, state){
     const requestData = {event: 'move', date: Date.now(), data:{direction:direction, state: state}};
     console.log('Move: ',requestData);
-    this.state.socketStream.send(JSON.stringify(requestData));
+    this.socketStream.send(JSON.stringify(requestData));
     this.setState({moveDirection: direction, moveState: state});
   }
 
   _protectHome(state){
     console.log('ProtectHome state: ', state);
     const requestData = {event: 'protectHome', date: Date.now(), data:{state: state}};
-    this.state.socketStream.send(JSON.stringify(requestData));
+    this.socketStream.send(JSON.stringify(requestData));
     this.setState({protectHomeState: state});
   }
 
@@ -62,37 +62,45 @@ export default class HalClient extends Component {
     return (
       <View style={styles.container}>
       <Text style={styles.socketInfo}>
-      SocketInfo: { this.state.socketStream.readyState }
+      SocketResponse: { this.state.socketResponse }
       </Text>
       <Text>
       Direction: {this.state.moveDirection}
       State: {this.state.moveState}
       </Text>
-      <Switch onValueChange={()=>{this._protectHome(value)}} value = {this.state.protectHomeState}/>
+      <Switch onValueChange={(value)=>{this._protectHome(value)}} value = {this.state.protectHomeState}/>
       <Button onPress={()=>this._connectSocket()} title="Connect socket"/>
       <TouchableWithoutFeedback 
-        onPressIn={()=>this._move('up', true)} onPressOut={()=>)this._move('up', false)}>
-      <Text>
-      UP
-      </Text>
+        onPressIn={()=>this._move('up', true)} onPressOut={()=>this._move('up', false)}>
+      <View style={styles.button}>
+        <Text>
+        UP
+        </Text>
+      </View>
       </TouchableWithoutFeedback>      
       <TouchableWithoutFeedback 
-        onPressIn={()=>this._move('down', true)} onPressOut={()=>)this._move('down', false)}>
-      <Text>
-      DOWN
-      </Text>
+        onPressIn={()=>this._move('down', true)} onPressOut={()=>this._move('down', false)}>
+      <View style={styles.button}>
+        <Text>
+          DOWN
+        </Text>
+      </View>
       </TouchableWithoutFeedback>      
       <TouchableWithoutFeedback 
-        onPressIn={()=>this._move('left', true)} onPressOut={()=>)this._move('left', false)}>
+        onPressIn={()=>this._move('left', true)} onPressOut={()=>this._move('left', false)}>
+      <View style={styles.button}>
       <Text>
-      LEFT
+        LEFT
       </Text>
+      </View>
       </TouchableWithoutFeedback>      
       <TouchableWithoutFeedback 
-        onPressIn={()=>this._move('right', true)} onPressOut={()=>)this._move('right', false)}>
-      <Text>
-      RIGHT
-      </Text>
+        onPressIn={()=>this._move('right', true)} onPressOut={()=>this._move('right', false)}>
+      <View style={styles.button}>
+        <Text>
+          RIGHT
+        </Text>
+      </View>
       </TouchableWithoutFeedback>
       </View>
       );
@@ -108,8 +116,16 @@ const styles = StyleSheet.create({
   },
   socketInfo: {
     fontSize: 20,
-    textAlign: 'center',
+    alignItems: 'center',
     margin: 10,
+  },
+  button: {
+    alignItems: 'center',
+    marginBottom: 7,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#000',
+    padding: 10
   },
 
 });

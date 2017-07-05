779a5eb7422cf9a6f2c4c42e2162da251aa5f17b
Cieniu <marcin@laret.pl>
2017-07-05 21:24:44 +0200

    debug

--- home_protect.py
+++ home_protect.py
@@ -76,10 +76,10 @@ class HomeProtect():
     def start(self):
         websocket.enableTrace(True)
         self.ws = websocket.WebSocketApp("ws://192.168.1.135:8083",
-                          on_message = on_message,
-                          on_error = on_error,
-                          on_close = on_close)
-        self.ws.on_open = on_open
+                          on_message = self.on_message,
+                          on_error = self.on_error,
+                          on_close = self.on_close)
+        self.ws.on_open = self.on_open
         self.ws.run_forever()
 
 

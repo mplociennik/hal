54bf9e6fae5c025e6fc0a05b17bedc50c9da92dc
Cieniu <marcin@laret.pl>
2017-07-14 19:10:58 +0200

    pir

--- pir.py
+++ pir.py
@@ -8,13 +8,13 @@ pir_sensor = 26
 GPIO.setup(pir_sensor, GPIO.IN, GPIO.PUD_DOWN)
 
 current_state = 0
-
+print("PIR sensor listening...")
 while True:
     try:
         time.sleep(0.1)
         current_state = GPIO.input(pir_sensor)
         if current_state == 1:
           print("GPIO pin %s is %s" % (pir_sensor, current_state))
-          time.sleep(4) # wait 4 seconds for PIR to reset. 
+          time.sleep(1) # wait 4 seconds for PIR to reset. 
     except KeyboardInterrupt:
         GPIO.cleanup()

f88a4c1d8d9e01bda78958256f8122d019e75661
Cieniu <marcin@laret.pl>
2017-07-15 15:08:03 +0200

    debug

--- loader.sh
+++ loader.sh
@@ -1,13 +1,13 @@
 #!/bin/sh 
-# test
 cd /
 cd home/pi/hal
 
 #update scripts
 git pull
 
-# robot move
-sudo python robot_move.py & > robot_move.log
-
 # home protect
-sudo python home_protect.py & > home_protect.log
+python home_protect.py & > home_protect.log
+
+# robot move
+python robot_move.py & > robot_move.log
+

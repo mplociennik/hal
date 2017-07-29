ccbc0c0124782a0b3e5385155d09290fa198ef6b
Cieniu <marcin@laret.pl>
2017-07-29 19:14:45 +0200

    debug

--- loader.py
+++ loader.py
@@ -1,7 +1,7 @@
 import os
 import time
 
-robot_move = os.popen('python robot_move.py')
-home_protect = os.popen('python home_protect.py')
-autopilot = os.popen('python autopilot.py')
-robot_camera = os.popen('python robot_camera.py')
+robot_move = os.popen('python /home/pi/hal/robot_move.py')
+home_protect = os.popen('python /home/pi/hal/home_protect.py')
+autopilot = os.popen('python /home/pi/hal/autopilot.py')
+robot_camera = os.popen('python /home/pi/hal/robot_camera.py')

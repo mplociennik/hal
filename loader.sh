b80ae93f4fdc7b21442e1f117b11643688eeffbd
Cieniu <marcin@laret.pl>
2017-07-29 13:06:50 +0200

    change loader

--- loader.sh
+++ loader.sh
@@ -6,10 +6,13 @@ cd home/pi/hal
 #update scripts
 git pull
 
-# home protect
-python home_protect.py &
-
 # robot move
-python robot_move.py &
+python robot_move.py
+
+# autopilot
+python autopilot.py
+
+# home protect
+python home_protect.py
 
 

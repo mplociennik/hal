8c5a6076044d09101c264b5db4885ac9dacd53a2
Cieniu <marcin@laret.pl>
2017-07-29 18:22:52 +0200

    debug

--- cameratest.py
+++ cameratest.py
@@ -2,7 +2,7 @@ from picamera import PiCamera
 from time import sleep
 
 camera = PiCamera()
-camera.resolution = (2592, 1944)
+# camera.resolution = (2592, 1944)
 camera.capture('/home/pi/Desktop/image.jpg')
 
 camera.start_recording('/home/pi/Desktop/video.h264')

42abebfe45ab43b845909ef2f1ed90e137fd4396
Cieniu <marcin@laret.pl>
2017-07-30 09:54:34 +0200

    debug

--- HalRobot/tests/test_robot_camera.py
+++ HalRobot/tests/test_robot_camera.py
@@ -1,16 +1,21 @@
 import unittest
+import os
+import datetime
+from robot_camera import RobotCamera
 
  
 class TestRobotCamera(unittest.TestCase):
  
     def setUp(self):
+        self.robot_camera = RobotCamera()
         pass
  
     def test_receive_commands(self):
         self.assertEqual( multiply(3,4), 12)
  
     def test_create_photo(self):
-        self.assertEqual( multiply('a',3), 'aaa')
+        phoo_object = self.robot_camera.get_photo()
+        self.assertTrue( os.path.isfile(photo_object.name))
 
     def test_send_photo(self):
         self.assertEqual( multiply('a',3), 'aaa')

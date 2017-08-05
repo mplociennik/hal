c9460c6cc4f18fbb480a7c3688dfd582f763f659
Cieniu <marcin@laret.pl>
2017-08-05 09:49:15 +0200

    update loader

--- HalRobot/loader.py
+++ HalRobot/loader.py
@@ -1,5 +1,5 @@
 import os
-import time
+from time import sleep
 from robot_hardware import RobotHardware
 
 LOADER_ENABLED = True
@@ -7,14 +7,14 @@ LOADER_ENABLED = True
 if LOADER_ENABLED:
     robot_hardware = RobotHardware()
     print robot_hardware.disable_hdmi()
-    time.sleep(5)
+    sleep(5)
     robot_move = os.popen('python /home/pi/hal/HalRobot/robot_move.py')
-    time.sleep(1)
+    sleep(1)
     home_protect = os.popen('python /home/pi/hal/HalRobot/robot_home_protect.py')
-    time.sleep(1)
+    sleep(1)
     autopilot = os.popen('python /home/pi/hal/HalRobot/robot_autopilot.py')
-    time.sleep(1)
+    sleep(1)
     robot_camera = os.popen('python /home/pi/hal/HalRobot/robot_camera.py')
-    time.sleep(1)
+    sleep(1)
 else:
     print('Loader is disabled!')

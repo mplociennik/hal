9bc332ef3c7ef570b3f489b5c96021d65c424d7a
Marcin Płóciennik <mplociennik@trans.eu>
2017-08-23 14:57:44 +0200

    speech

--- HalRobot/speech.py
+++ HalRobot/speech.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
 import urllib2
-import pyvona
+import md5
+import base64
+import voicerss_tts
 import multiprocessing
 from audio import Audio
 
-
-IVONA_ACCESS_KEY = 'GDNAIKZKKGPM3SPFPZGA'
-IVONA_SECRET_KEY = 'PXnXmq3aV1qYsV4jxG4WtoVhESq4gZaXGjrDTBke'
+API_KEY = '481c2544951b469db5ef701015479b2e'
+SPEECH_DIR_NAME = 'speech'
+VOICES_DIR_NAME = '{0}/voices'.format(SPEECH_DIR_NAME)
 
 class Speech(multiprocessing.Process):
     """Class to making connection to voice webapi."""
 
-    name = 'Joey'
-    region = 'eu-east'
+    language = 'en-us'
         
     def __init__(self, name=None, region=None):
+        if not os.path.isdir(SPEECH_DIR_NAME):
+            os.mkdir(SPEECH_DIR_NAME)
+        if not os.path.isdir(VOICES_DIR_NAME):
+            os.mkdir(VOICES_DIR_NAME)
+
         multiprocessing.Process.__init__(self)
         self.exit = multiprocessing.Event()
-        if name:
-            self.name = name
-        if region:
-            self.region = region
         
     def terminate(self):
         self.exit.set() 
@@ -32,19 +35,41 @@ class Speech(multiprocessing.Process):
     def filter_spaces(self, text):
         return text.replace(" ", "%20")
 
-    def create_voice(self, text):
-        print 'creating voice'
-        v = pyvona.create_voice(IVONA_ACCESS_KEY, IVONA_SECRET_KEY)
-        v.voice_name = self.name
-        v.region = self.region
-        try:
-            v.speak(text)
-        except:
-            print "Speech: connection not found!"
+    def create_voice(self, text, lang=language, format='mp3', rate='44khz_16bit_stereo', ssl='false', b64='true'):
+        print 'creating voice...'
+        # try:
+        voice = voicerss_tts.speech({
+            'key': API_KEY,
+            'hl': lang,
+            'src': text,
+            'r': '0',
+            'c': format,
+            'f': rate,
+            'ssml': ssl,
+            'b64': b64
+        })
+        print('<audio src="' + voice['response'] + '" autoplay="autoplay"></audio>')
+        m = md5.new()
+        m.update(text)
+        textHash = m.hexdigest()
+        fileNamePath = '{0}/{1}.{2}'.format(VOICES_DIR_NAME,textHash, format)
+        filename = self.write_voice(fileNamePath,  base64.b64decode(voice['response']))
+        #     print(filename)
+        # except:
+        #     print "Speech: ERROR!"
+
+    def write_voice(self, fileNamePath, voice_binary):
+        # try:
+        with open(fileNamePath, 'w') as f:
+            f.write(voice_binary)
+            f.close()
+        # except:
+        # print('Cannot create file "{0}"'.format(fileNamePath))
+        return fileNamePath
 
     def play_sound(self, file):
         Audio(file, 1.0)
 
 if __name__ == "__main__":
     speech = Speech()
-    speech.hello('Hello world!')
+    speech.hello('Hello world man! How are you?')

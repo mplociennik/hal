dbbb032b1aa495b197e333fe86eb963c431a0258
Cieniu <marcin@laret.pl>
2017-08-24 22:23:26 +0200

    test

--- HalRobot/camera_recorder.py
+++ HalRobot/camera_recorder.py
@@ -3,12 +3,3 @@ import picamera
 camera = picamera.PiCamera()
 camera.start_preview()
 camera.start_recording('video.h264')
-
-try:
-    camera = picamera.PiCamera()
-    camera.start_preview()
-    camera.start_recording('video.h264')
-except KeyboardInterrupt:
-    raise
-except:
-    camera.stop_recording()

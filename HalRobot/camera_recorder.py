5804b9719e57e45831603f80588a90dd0d326f2c
Cieniu <marcin@laret.pl>
2017-08-24 22:28:16 +0200

    test

--- HalRobot/camera_recorder.py
+++ HalRobot/camera_recorder.py
@@ -2,6 +2,7 @@ import picamera
 
 camera = picamera.PiCamera()
 camera.resolution = (640, 480)
-camera.start_recording('/home/pi/Desktop/my_video.h264')
-camera.wait_recording(60)
-camera.stop_recording()
+camera.start_preview()
+# camera.start_recording('/home/pi/Desktop/my_video.h264')
+# camera.wait_recording(60)
+# camera.stop_recording()

83d65958ac25acbb29687592a09cfc855ad38f81
Cieniu <marcin@laret.pl>
2017-09-02 13:00:53 +0200

    fix

--- HalRobot/speech.py
+++ HalRobot/speech.py
@@ -94,7 +94,7 @@ class Speech(multiprocessing.Process):
     def say(self, text):
         result = self.search_voice(text)
         print(result.hash)
-        if result:
+        if result is not None:
             fileNamePath = self.get_file_name_path(result.hash)
         else:
             fileNamePath = self.create_voice(text)

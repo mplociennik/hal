d295e2b414468f7114291b1f5332a0d299553f00
Cieniu <marcin@laret.pl>
2017-09-08 14:11:01 +0200

    debug

--- HalRobot/robot_camera.py
+++ HalRobot/robot_camera.py
@@ -3,7 +3,7 @@ import os
 import json
 from robot_websocket_client import RobotWebsocketClient
 STREAM_CMD = "raspivid -o - -t 0 -w 800 -h 400 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/x}' :demux=h264 &"
-STOP_CMD = "killall raspivid"
+STOP_CMD = "killall raspivid && killall cvlc"
 
 
 class RobotCamera(RobotWebsocketClient):

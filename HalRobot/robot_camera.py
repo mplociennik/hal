f9fb72864e09a342da2fcf7bae52e20a108a3b9b
Cieniu <marcin@laret.pl>
2017-09-11 20:47:03 +0200

    debug

--- HalRobot/robot_camera.py
+++ HalRobot/robot_camera.py
@@ -2,7 +2,7 @@
 import os
 import json
 from robot_websocket_client import RobotWebsocketClient
-STREAM_CMD = "sudo raspivid -o - -t 0 -w 800 -h 400 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/x}' :demux=h264 &"
+STREAM_CMD = "./stream_camera.sh"
 STOP_CMD = "killall raspivid && killall cvlc"
 
 
